fd778b983dd6215300b28e29b9fc1f3675ff1ba6
zhuBoDian <379837872@qq.com>
2023-01-31 11:28:00 +0800

    34在排序数组中查找元素的第一个和最后一个位置

---
+++ array/34.cpp
@@ -0,0 +1,69 @@
+/*
+34. 在排序数组中查找元素的第一个和最后一个位置
+*/
+
+//头文件
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+//leetcode代码
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int leftBorder = getLeftBorder(nums, target);
+        int rightBorder = getRightBorder(nums, target);
+        // 情况一
+        if (leftBorder == -2 || rightBorder == -2) return {-1, -1};
+        // 情况三
+        if (rightBorder - leftBorder > 1) return {leftBorder + 1, rightBorder - 1};
+        // 情况二
+        return {-1, -1};
+    }
+private:
+     int getRightBorder(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        int rightBorder = -2; // 记录一下rightBorder没有被赋值的情况
+        while (left <= right) {
+            int middle = left + ((right - left) / 2);
+            if (nums[middle] > target) {
+                right = middle - 1;
+            } else { // 寻找右边界，nums[middle] == target的时候更新left
+                left = middle + 1;
+                rightBorder = left;
+            }
+        }
+        return rightBorder;
+    }
+    int getLeftBorder(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        int leftBorder = -2; // 记录一下leftBorder没有被赋值的情况
+        while (left <= right) {
+            int middle = left + ((right - left) / 2);
+            if (nums[middle] >= target) { // 寻找左边界，nums[middle] == target的时候更新right
+                right = middle - 1;
+                leftBorder = right;
+            } else {
+                left = middle + 1;
+            }
+        }
+        return leftBorder;
+    }
+};
+
+
+//主函数
+int main() {
+    vector<int> nums{-1, 0, 3, 5, 9, 12};
+    int target = 9;
+
+    Solution solution;
+    // cout << solution.searchRange(nums, target) << endl;
+
+
+    return 0;
+}
