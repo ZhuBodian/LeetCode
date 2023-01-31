75d651e1e3a23ec96ea61de72f9abcfbde979a2c
zhuBoDian <379837872@qq.com>
2023-01-31 09:37:49 +0800

    二分查找

--- array/704.cpp
+++ array/704.cpp
@@ -1,10 +1,10 @@
 /* 704 二分查找
 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
 
-来源：力扣（LeetCode）
-链接：https://leetcode.cn/problems/binary-search
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
-
+提示：
+你可以假设 nums 中的所有元素是不重复的。
+n 将在 [1, 10000]之间。
+nums 的每个元素都将在 [-9999, 9999]之间。
 
 */
 
@@ -15,7 +15,7 @@
 using namespace std;
  
 //leetcode代码
-// 版本一
+// 版本一，左闭右闭
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
@@ -35,6 +35,29 @@ public:
         return -1;
     }
 };
+
+
+// 版本二，左闭右开
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size(); // 定义target在左闭右开的区间里，即：[left, right)
+        while (left < right) { // 因为left == right的时候，在[left, right)是无效的空间，所以使用 <
+            int middle = left + ((right - left) >> 1);
+            if (nums[middle] > target) {
+                right = middle; // target 在左区间，在[left, middle)中
+            } else if (nums[middle] < target) {
+                left = middle + 1; // target 在右区间，在[middle + 1, right)中
+            } else { // nums[middle] == target
+                return middle; // 数组中找到目标值，直接返回下标
+            }
+        }
+        // 未找到目标值
+        return -1;
+    }
+};
+
 
 
 //主函数
