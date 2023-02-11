39b34863156b17b3193bc9ff5a2f9d9f17949d49
zhuBoDian <379837872@qq.com>
2023-02-11 16:22:32 +0800

    1

---
+++ hash/30.cpp
@@ -0,0 +1,61 @@
+/*30. 串联所有单词的子串
+
+*/
+
+//头文件
+#include <unordered_map>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+//leetcode代码
+class Solution {
+public:
+    vector<int> findSubstring(string &s, vector<string> &words) {
+        vector<int> res;
+        int m = words.size(), n = words[0].size(), ls = s.size();
+        for (int i = 0; i < n && i + m * n <= ls; ++i) {
+            unordered_map<string, int> differ;
+            for (int j = 0; j < m; ++j) {
+                ++differ[s.substr(i + j * n, n)];
+            }
+            for (string &word: words) {
+                if (--differ[word] == 0) {
+                    differ.erase(word);
+                }
+            }
+            for (int start = i; start < ls - m * n + 1; start += n) {
+                if (start != i) {
+                    string word = s.substr(start + (m - 1) * n, n);
+                    if (++differ[word] == 0) {
+                        differ.erase(word);
+                    }
+                    word = s.substr(start - n, n);
+                    if (--differ[word] == 0) {
+                        differ.erase(word);
+                    }
+                }
+                if (differ.empty()) {
+                    res.emplace_back(start);
+                }
+            }
+        }
+        return res;
+    }
+};
+
+
+//主函数
+int main() {
+    vector<string> words {"foo","bar"};
+    string s = "barfoothefoobarman";
+
+    Solution solution;
+    solution.findSubstring(s, words);
+    //cout << solution.findSubstring(s, words) << endl;
+
+
+    return 0;
+}
